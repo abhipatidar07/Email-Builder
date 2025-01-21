1a44f8778a96c9142d05122f9380bc36231dcd39
abhipatidar07 <patidarabhishek831@gmail.com>
2025-01-21 16:29:22 +0530

    seven

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(
   cors({
-    origin: "*",
+    // origin: "*",
+    origin: process.env.NODE_ENV === "production" ? "https://email-builder-five-wheat.vercel.app" : "*",
     credentials: true,
   })
 );
@@ -30,11 +31,17 @@ const upload = multer({ dest: "uploads/" });
 //   }`;
 //   res.json({ url: imageUrl });
 // });
+// app.post("/uploadImage", upload.single("image"), (req, res) => {
+//   const file = req.file;
+//   // Dynamically replace the protocol and host if in production
+//   const host = process.env.NODE_ENV === "production" ? "https://email-builder-b1og.onrender.com" : req.get("host");
+//   const imageUrl = `${req.protocol}://${host}/uploads/${file.filename}`;
+//   res.json({ url: imageUrl });
+// });
 app.post("/uploadImage", upload.single("image"), (req, res) => {
   const file = req.file;
-  // Dynamically replace the protocol and host if in production
-  const host = process.env.NODE_ENV === "production" ? "https://email-builder-b1og.onrender.com" : req.get("host");
-  const imageUrl = `${req.protocol}://${host}/uploads/${file.filename}`;
+  const protocol = process.env.NODE_ENV === 'production' ? 'https' : req.protocol; // Handle HTTP/HTTPS based on environment
+  const imageUrl = `${protocol}://${req.get("host")}/uploads/${file.filename}`;
   res.json({ url: imageUrl });
 });
 
